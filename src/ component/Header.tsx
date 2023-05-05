d515b854b61b9dfd6d426d39886dc4f5cb4da1ae
Nikudanngo <satoru_rutosa@icloud.com>
2023-05-06 00:55:43 +0900

    header追加

---
+++ src/ component/Header.tsx
@@ -0,0 +1,20 @@
+export const Header = () => {
+  return (
+    <>
+      <img src="/mac_header_sample.png" alt="header_sample" />
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#works">Works</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
