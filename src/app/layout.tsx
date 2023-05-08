ed59ec30117abdece8124c03d364009f6bf75beb
Nikudanngo <satoru_rutosa@icloud.com>
2023-05-08 16:07:39 +0900

    no use delete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
-import { Header } from "@/ component/Header";
+import { Header } from "@/components/Header";
 import "./globals.css";
-import { Inter } from "next/font/google";
-import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "onikumaru.com",
@@ -18,13 +14,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      {/* <Head>
-        <title>{metadata.title}</title>
-        <meta charSet="utf-8" />
-        <meta name="description" content={metadata.description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head> */}
       <body>
         <Header />
         {children}
