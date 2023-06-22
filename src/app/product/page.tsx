f59d12094d1f62c37853c2bdefe50186113735e9
Nikudanngo <satoru_rutosa@icloud.com>
2023-06-23 00:25:30 +0900

    productの追加とlist化

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,51 +1,30 @@
 "use client";
 
 import { Notion } from "@/components/os/Notion";
+import { products } from "@/type";
+import classNames from "classnames";
 
 export default function Product() {
   return (
     <Notion>
-      <div className="mx-auto w-1/2">
+      <div className="mx-auto w-2/3">
         <h2>Product</h2>
         <p>ここでは私が今まで制作してきたプロダクトを紹介します。</p>
-        <h2>Portfolio</h2>
       </div>
-      <div className="z-20 flex gap-4 overflow-x-auto">
-        <div className="rounded-xl border-2 border-notion-text/20 p-1">
-          <div className="">
-            <img
-              src="/VRoid.jpg"
-              alt="VRoid"
-              className="h-[300px] min-w-[400px] rounded-xl object-cover object-top"
-            />
-          </div>
-          <div>
-            <p>3Dアバターの制作販売</p>
-          </div>
-        </div>
-        <div className="rounded-xl border-2 border-notion-text/20 p-1">
-          <div className="">
-            <img
-              src="/ThoutOfOcean.jpg"
-              alt="VRoid"
-              className="h-[300px] w-[400px] min-w-[400px] rounded-xl object-cover"
-            />
-          </div>
-          <div>
-            <p>竹歯ブラシ販売サイト</p>
-          </div>
-        </div>
-        <div className="rounded-xl border-2 border-notion-text/20 p-1">
-          <div className="">
-            <img
-              src="/ThoutOfOcean.jpg"
-              alt="VRoid"
-              className="h-[300px] w-[400px] min-w-[400px] rounded-xl object-cover"
-            />
-          </div>
-          <div>
-            <p>学食モバイルオーダー</p>
-          </div>
+      <div className="mx-auto w-3/4">
+        <div className="grid grid-cols-6 gap-6">
+          {products.map((product) => (
+            <div
+              key={product.title}
+              className={classNames("rounded-xl border", product.className)}
+            >
+              <img
+                src={product.image}
+                alt={product.alt}
+                className="h-full rounded-xl object-cover"
+              />
+            </div>
+          ))}
         </div>
       </div>
     </Notion>
--- src/type/index.ts
+++ src/type/index.ts
@@ -1,6 +1,24 @@
+import { HTMLAttributes } from "react";
+
 export const PageNamePath = [
   { name: "詳細", path: "/about" },
   { name: "作品", path: "/product" },
   { name: "連絡先", path: "/contact" },
 ];
 
+type GridArea = "leftTop" | "centerTop" | "rightTop" | "leftCenter" | "centerCenter" | "rightCenter" | "leftBottom" | "centerBottom" | "rightBottom";
+
+interface Product {
+  title: string;
+  image: string;
+  className?: string;
+  alt: string;
+  description: string;
+}
+
+export const products: Product[] = [
+  { title: "3Dアバターの制作販売", className: "col-span-2", image: "/Vroid.jpg", alt: "VRoid", description: "Vroidで作成した3Dアバターを販売しています。" },
+  { title: "学食モバイルオーダー", className: "col-span-4", image: "/thumbnail-run-ticket.png", alt: "RunTicket", description: "学食のモバイルオーダーシステムを作成しました。" },
+  { title: "竹歯ブラシ販売サイト", className: "col-span-4", image: "/ThoutOfOcean.jpg", alt: "KAGUYA", description: "竹歯ブラシの販売サイトを作成しました。" },
+  { title: "ポートフォリオサイト", className: "col-span-2", image: "/my-portfolio.png", alt: "my-portfolio", description: "このサイトです。" },
+];
