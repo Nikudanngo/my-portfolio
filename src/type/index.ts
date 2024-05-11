1016e0ce6e6619ccf75a3108aa7da71368319596
nnikudango <nnikudango@gmail.com>
2024-05-11 16:41:19 +0900

    add link

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,3 +1,5 @@
+import { Url } from "next/dist/shared/lib/router/router";
+
 export const PageNamePath = [
   { name: "詳細", path: "/about" },
   { name: "作品", path: "/product" },
@@ -11,11 +13,12 @@ interface Product {
   colSpan: number;
   alt: string;
   description: string;
+  link?: Url;
 }
 
 export const products: Product[] = [
-  { title: "3Dアバターの制作販売", colSpan: 2, image: "/VRoid.jpg", alt: "VRoid", description: "Vroidで作成した3Dアバターを販売しています。" },
-  { title: "学食モバイルオーダー", colSpan: 4, image: "/thumbnail-run-ticket.png", alt: "RunTicket", description: "学食のモバイルオーダーシステムを作成しました。" },
-  { title: "竹歯ブラシ販売サイト", colSpan: 4, image: "/ThoutOfOcean.jpg", alt: "KAGUYA", description: "竹歯ブラシの販売サイトを作成しました。" },
-  { title: "ポートフォリオサイト", colSpan: 2, image: "/my-portfolio.png", alt: "my-portfolio", description: "このサイトです。" },
+  { title: "3Dアバターの制作販売", colSpan: 2, image: "/VRoid.jpg", alt: "VRoid", description: "Vroidで作成した3Dアバターを販売しています。", link: "https://coconala.com/services/1624823" },
+  { title: "学食モバイルオーダー", colSpan: 4, image: "/thumbnail-run-ticket.png", alt: "RunTicket", description: "学食のモバイルオーダーシステムを作成しました。", link: "https://product.run-ticket.com/" },
+  { title: "竹歯ブラシ販売サイト", colSpan: 4, image: "/ThoutOfOcean.jpg", alt: "KAGUYA", description: "竹歯ブラシの販売サイトを作成しました。", link: "https://www.thoughtofoceans.com/" },
+  { title: "ポートフォリオサイト簡易版", colSpan: 2, image: "/my-portfolio.png", alt: "my-portfolio-light", description: "このサイトです。", link: "https://www.onikumaru.com" },
 ];
